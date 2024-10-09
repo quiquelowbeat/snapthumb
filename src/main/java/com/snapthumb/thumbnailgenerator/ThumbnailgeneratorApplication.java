3eaab78a8273c85a3c64316db30bd72fcf223a62
quiquelowbeat <kikeebcn@gmail.com>
2024-10-09 16:02:35 +0200

    chore: initialize project
    - Add basic project structure
    - Include initial configuration files
    - Create README with project description

---
+++ src/main/java/com/snapthumb/thumbnailgenerator/ThumbnailgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.snapthumb.thumbnailgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbnailgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThumbnailgeneratorApplication.class, args);
+	}
+
+}
